ea86042f308d1c5585073a021e68de47fe98860d
D5Exp <112584814+D5Exp@users.noreply.github.com>
2022-11-15 22:55:20 +0200

    Update train_model.py
    1st API attempt

--- utils/train_model.py
+++ utils/train_model.py
@@ -12,20 +12,25 @@
 # Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 # Fetch training data and preprocess for modeling
 train = pd.read_csv('./data/df_train.csv')
 
 y_train = train[['load_shortfall_3h']]
-X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+#X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+X_train = train.drop(['load_shortfall_3h'],axis=1)
 
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+#lm_regression = LinearRegression(normalize=True)
+RF_reg =RandomForestRegressor(n_estimators=2500,max_depth=50, random_state=0)
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+#lm_regression.fit(X_train, y_train)
+RF_reg.fit(X_train, y_train)
 
 # Pickle model for use within our API
 save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+#pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(RF_reg, open(save_path,'wb'))
